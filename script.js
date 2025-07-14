8b3d9a86f67983a94434cd0bd221c63377315a12
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-07-14 12:49:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 
+
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
