e0985691ff97aa920af7bdaec811b153b5e77ea7
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-07-14 13:42:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ hamburger.addEventListener('click', () => {
 });
 
 
-function openLeftMenu() {
-  document.getElementById("leftMenu").style.display = "block";
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
 }
 
-function closeLeftMenu() {
-  document.getElementById("leftMenu").style.display = "none";
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
 }
